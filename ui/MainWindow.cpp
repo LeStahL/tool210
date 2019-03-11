198805ea341229b010475e6d72899a977f33a535
Alexander Kraus <nr4@z10.info>
2019-03-11 21:13:35 +0100

    added simple window

---
+++ ui/MainWindow.cpp
@@ -0,0 +1,34 @@
+/* Team210 Demotool 
+ * Copyright (C) 2019  Alexander Kraus <nr4@z10.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "MainWindow.hpp"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , m_ui(new Ui::MainWindow)
+{
+    m_ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete m_ui;
+}
+
+
+
